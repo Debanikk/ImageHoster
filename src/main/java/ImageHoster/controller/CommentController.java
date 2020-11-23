705276513da496db334a7a93851df12ea6a341d4
Debanikk <debanikkundu2014@gmail.com>
2020-11-23 21:11:38 +0530

    First Milestone

---
+++ src/main/java/ImageHoster/controller/CommentController.java
@@ -0,0 +1,32 @@
+package ImageHoster.controller;
+
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import ImageHoster.model.User;
+import ImageHoster.service.CommentService;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @RequestMapping(value="/image/{image.Id}/{image.Title}/comments", method = RequestMethod.POST)
+    public String uploadComment(@RequestParam("comment") String comment, @RequestParam("image") Image image , HttpSession session){
+        User user = (User) session.getAttribute("loggeduser");
+
+        return "index";
+    }
+}
---
+++ src/main/java/ImageHoster/service/CommentService.java
@@ -0,0 +1,9 @@
+package ImageHoster.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+
+}
